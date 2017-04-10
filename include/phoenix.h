0472c1402a761cc7a366b1d73c1e82586776f7c1
Pradeep Fernando <pradeepfn@gmail.com>
2017-04-10 19:54:28 -0400

    ported to c++ successfully -gtc and miniAMR works

--- include/phoenix.h
+++ include/phoenix.h
@@ -3,21 +3,30 @@
 
 #include <stdint.h>
 
-typedef struct px_obj_{
-	void *data;
-	unsigned long size;
-	long version;
-}px_obj;
 
-int px_init(int proc_id);
-int px_create(char *key1, unsigned long size,px_obj *retobj);
-int px_get(char *key1, uint64_t version, px_obj *retobj);
-int px_deltaget(char *key1,uint64_t version, px_obj *retobj);
-int px_commit(char *key1,int version);
-int px_snapshot();
-int px_get_snapshot();
-int px_delete(char *key1);
-int px_finalize();
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	typedef struct px_obj_{
+		void *data;
+		unsigned long size;
+		long version;
+	}px_obj;
+
+	int px_init(int proc_id);
+	int px_create(char *key1, unsigned long size,px_obj *retobj);
+	int px_get(char *key1, uint64_t version, px_obj *retobj);
+	int px_deltaget(char *key1,uint64_t version, px_obj *retobj);
+	int px_commit(char *key1,int version);
+	int px_snapshot();
+	int px_get_snapshot();
+	int px_delete(char *key1);
+	int px_finalize();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
