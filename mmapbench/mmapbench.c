c58acad87990ae028cad2a090420fd303bceb9de
Pradeep Fernando <pradeepfn@gmail.com>
2017-09-29 15:44:34 -0400

    almost there with runs

--- mmapbench/mmapbench.c
+++ mmapbench/mmapbench.c
@@ -42,12 +42,43 @@
  int nbufs = 128000; 
 char *shared_area = NULL;
 int flag[32];
-int ncores = 4;
-char *filename = "/mnt/pmfs/share.dat";
+int ncores = 1;
+//char *filename = "/mnt/pmfs/share.dat";
+char *filename = "/mnt/mem/share.dat";
+
+#ifdef __YUMA
+uint64_t filesize = 1 * 1024 * 1024;  //1MB
+uint64_t mmapsize = 1 * 1024 * 1024 *1024; // 1 GB
+int iosize = -1; // provided as a cmd argument
+void *data_chunk = NULL; // data chunk to be copied
+#endif
 
 void *
-worker(void *args)
+memcpy_worker(void *args)
 {
+    int id = (long) args;
+    int ret = 0;
+    uint64_t i;
+	uint64_t j;
+
+    affinity_set(id);
+
+	uint64_t nwrites = mmapsize/filesize;
+	uint64_t nchunks = filesize/iosize;
+	assert(iosize != -1);
+
+    for (i = 0; i < nwrites; i++){
+		for(j=0; j < nchunks; j++){ 
+			memcpy(&shared_area[i*filesize + j*iosize],data_chunk,iosize);
+		}
+		//msync
+	}
+    //printf("potato_test: thread#%d done.\n", core);
+    return (void *) 0;
+}
+
+void *
+worker(void *args){
     int id = (long) args;
     int ret = 0;
     int i;
@@ -59,7 +90,6 @@ worker(void *args)
     //printf("potato_test: thread#%d done.\n", core);
     return (void *) (long) ret;
 }
-
 
 struct nvmap_arg_struct{
 
@@ -117,14 +147,24 @@ main(int argc, char **argv)
     for (i = 0; i < ncores; i++) {
         flag[i] = 0;
     }
-
+#ifdef __YUMA
     if (argc > 1) {
         ncores = atoi(argv[1]);
     }
+#else
+    if (argc > 1) {
+        iosize = atoi(argv[1]);
+    }
+#endif
 #ifdef USEFILE
+#ifdef _YUMA
+    create_file(filename, (1 + nbufs) * 4096);
+    fd = open(filename, O_RDONLY);
+#else
     create_file(filename, (1 + nbufs) * 4096);
     fd = open(filename, O_RDONLY);
     a.fd = fd;
+#endif
 #else
     a.fd = -1;	 
 #endif
@@ -135,6 +175,11 @@ main(int argc, char **argv)
     a.pflags = 1;
     a.noPersist = 0;
     shared_area = mmap(0, (1 + nbufs) * 4096, PROT_READ, MAP_FLAGS, fd, 0);
+
+#ifdef _YUMA
+	data_chunk = malloc(iosize);
+	memset(data_chunk,1, iosize);
+#endif
     //shared_area = (char *) syscall(__NR_nv_mmap_pgoff, 0,(1 + nbufs) * 4096,  PROT_READ | PROT_WRITE, MAP_PRIVATE| MAP_ANONYMOUS, &a);
     if (shared_area == MAP_FAILED) {
             perror("Error mmapping the file");
@@ -144,7 +189,11 @@ main(int argc, char **argv)
          
     start = read_tsc();
     for (i = 0; i < ncores; i++) {
+#ifdef __YUMA
+        pthread_create(&tid[i], NULL, memcpy_worker, (void *) (long) i);
+#else
         pthread_create(&tid[i], NULL, worker, (void *) (long) i);
+#endif
     }
 
     for (i = 0; i < ncores; i++) {
