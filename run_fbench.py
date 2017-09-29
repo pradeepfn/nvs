1f39f2208d6b71cd63e62eab4be4b3a3d08114cc
Pradeep Fernando <pradeepfn@gmail.com>
2017-09-29 15:43:25 -0400

    script

--- run_fbench.py
+++ run_fbench.py
@@ -12,7 +12,7 @@ DBG=1
 __home = os.getcwd()
 __fbench_root = '/home/pradeep/checkout/nvm-yuma/yuma/bench-yuma'  # root of fbench script location
 
-__chunk_size = ['64' '256']
+__chunk_size = [64, 256]
 
 __empty = ''
 __newfile = 'newfile'
@@ -76,12 +76,28 @@ def sh(cmd):
 
 
 
-def fb(wl, iosize):
+def fb(wl, data):
 
+    __t = wl + '.template'
+    __out = wl + '.f'
+
+    #generate workload file from template
+    cd('bench-yuma')
+
+    template = open(__t, "rt").read()
+
+
+    with open(__out, "wt") as output:
+        output.write(template % data)
+
+    cd(__home)
     cmd = 'filebench'
-    cmd +=  ' -f ' + __fbench_root + '/' + wl
+    cmd +=  ' -f ' + __fbench_root + '/' + __out
     msg(cmd)
     sh(cmd)
+
+    #delete the generated file
+    os.remove(__fbench_root + '/' + __out)
 
     cd(__home)
 
@@ -89,13 +105,17 @@ def fb(wl, iosize):
 if __name__ == '__main__':
 
     w = args.workload
+    t_size = 1 * 1024 *1024
 
     if w == __newfile:
         for i in __chunk_size:
-            fb('write_newfile.f', i)
+            n = t_size/i
+            data = {"chunk_size": i, "nchunks": n}
+            fb('write_newfile', data)
     elif w == __bigfile:
         for i in __chunk_size:
-            fb('write_bigfile.f', i)
+            data = {"chunk_size": i}
+            fb('write_bigfile', data)
     elif w == __mmap:
         print "invalid"
     else:
