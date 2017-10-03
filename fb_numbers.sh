2642b2959855df6de121450e01bd6888c78aeb81
Waduge Pradeep Rangana Fernando <pfernando3@aries4.cc.gatech.edu>
2017-10-03 15:24:48 -0400

    aries run

--- fb_numbers.sh
+++ fb_numbers.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-declare -a bname=("newfile" "bigfile")
-declare -a chunksize=("64k" "128k" "256k")
+#declare -a bname=("newfile" "bigfile")
+declare -a bname=("mmap")
+declare -a chunksize=("64" "256" "1k" "4k" "16k" "64k" "256k" "1m")
 #declare -a chunksize=("64k")
-filesize=1g
+filesize=4g
 stepsize=1m
 rep=5
 
@@ -12,7 +13,7 @@ do
 	mkdir -p results/"$bm"
 	for cs in "${chunksize[@]}"
 	do
-		./run_fbench.py -w "$bm" -t "$filesize" -s "$stepsize" -c "$cs"  > results/"$bm"/"$bm"_f"$filesize"_s"$stepsize"_c"$cs".txt
+		./run_bench.py -w "$bm" -t "$filesize" -s "$stepsize" -c "$cs"  > results/"$bm"/"$bm"_f"$filesize"_s"$stepsize"_c"$cs".txt
 	done
 done
 exit
