5e3e8efd027bca134733d2ed5ec16df19b36601c
Waduge Pradeep Rangana Fernando <pfernando3@aries4.cc.gatech.edu>
2017-10-06 12:59:10 -0400

    minor

--- fb_numbers.sh
+++ fb_numbers.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-#declare -a bname=("newfile" "bigfile")
-declare -a bname=("mmap")
-declare -a chunksize=("64" "256" "1k" "4k" "16k" "64k" "256k" "1m")
-#declare -a chunksize=("64k")
-filesize=4g
+#declare -a bname=("bigfile")
+declare -a bname=("mmapnoflush")
+declare -a chunksize=("256" "1k" "4k" "16k" "64k" "256k" "1m")
+#declare -a chunksize=("1m")
+filesize=16g
 stepsize=1m
-rep=5
 
 for bm in "${bname[@]}"
 do
@@ -14,6 +13,10 @@ do
 	for cs in "${chunksize[@]}"
 	do
 		./run_bench.py -w "$bm" -t "$filesize" -s "$stepsize" -c "$cs"  > results/"$bm"/"$bm"_f"$filesize"_s"$stepsize"_c"$cs".txt
+		#./run_bench.py -w "$bm" -t "$filesize" -s "$stepsize" -c "$cs"
+		echo "removing the benchmark files"
+		rm -rf /mnt/pmfs/*
+		sleep 5
 	done
 done
 exit
