c594fa17c818fb98b96fc7b2f35abd3fd2c332c6
Pradeep Fernando <pradeepfn@gmail.com>
2017-10-30 10:19:35 -0400

    wrong adds

--- nvstream/build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pradeep/checkout/nvm-yuma/yuma/nvstream/test
-# Build directory: /home/pradeep/checkout/nvm-yuma/yuma/nvstream/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_rm "/home/pradeep/checkout/nvm-yuma/yuma/nvstream/build/test/test_rm")
--- nvstream/build/third-party/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pradeep/checkout/nvm-yuma/yuma/nvstream/third-party/gtest
-# Build directory: /home/pradeep/checkout/nvm-yuma/yuma/nvstream/build/third-party/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
