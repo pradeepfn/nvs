84d88bd33039d1c116703b9b89f387cd23cdc21f
Pradeep Fernando <pradeepfn@gmail.com>
2018-01-17 09:49:46 -0500

    run script

---
+++ microbench/src/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -rf /dev/shm/unity_NVS_ROOT*
+rm -rf /dev/shm/shm
+mpirun -np 1 --bind-to core /home/pradeep/nvs/nvstream/build/src/tools/nvsformat
+mpirun -np 8 --bind-to core ./main
