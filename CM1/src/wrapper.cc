71e970c5a263c93dadc7f8ad89187b09b0f6e865
Pradeep Fernando <pradeepfn@gmail.com>
2018-04-24 18:19:22 -0400

    porting cm1 to nvs

---
+++ CM1/src/wrapper.cc
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "nvs/store.h"
+#include "nvs/store_manager.h"
+
+#include "wrapper.h"
+
+#define STORE_ID "cm1_store"
+
+extern "C" {
+    nvs::Store *st;
+
+    int nvs_init_(int *proc_id) {
+        std::string store_name = std::string(STORE_ID) + std::string("/") +std::to_string(*proc_id);
+        st = nvs::StoreManager::GetInstance(store_name);
+    }
+
+
+    void *alloc(unsigned long *size, char *s) {
+        void *ptr;
+        st->create_obj(std::string(s),*size, &ptr);
+        return ptr;
+    }
+
+    void nvs_free_(void *ptr) {
+    }
+
+    void nvs_snapshot_(int *proc_id) {
+        st->put_all();
+    }
+
+    int nvs_finalize_() {
+        st->stats();
+    }
+}
